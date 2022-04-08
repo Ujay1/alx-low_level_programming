4a25189ccbeee1c9e9f8fc232fbea5641bab4627
Obianuju <vickielyon4@gmail.com>
2022-04-08 09:11:38 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various data type
- * Return: Always 0 (success)
+	 * main - prints the size of various data type
+	 * Return: Always 0 (success)
  */
 int main(void)
 {
