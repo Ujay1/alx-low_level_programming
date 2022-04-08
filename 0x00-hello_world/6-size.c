96783b75ce5b52860bdb344f2cfbae9ac803a793
Obianuju <vickielyon4@gmail.com>
2022-04-08 02:51:39 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ char d;
 float f;
 
 fprintf(stderr, "size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+fprintf(stderr, "size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+fprintf(stderr, "size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+fprintf(stderr, "size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+fprintf(stderr, "size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
