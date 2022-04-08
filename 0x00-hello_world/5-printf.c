c2d7d1ef6889e9be319cbea87130c506e66ed1f8
Obianuju <vickielyon4@gmail.com>
2022-04-07 17:45:37 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * My hello, printf task
- * Return: Always 0 (success)
+ *main - Entry point
+ *My hello, printf task
+ *Return: Always 0 (success)
  */
 int main(void)
 {
