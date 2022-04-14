6267e39475c0a8534960ab050fe9299c09cc296e
Obianuju <vickielyon4@gmail.com>
2022-04-13 19:52:29 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Printing alphabet in lowercase 
+ * Return: Always 0 as success
+ */
+void print_alphabet(void)
+{
+char d;
+for (d = 'a'; d <= 'z'; d++)
+_putchar(d);
+_putchar('\n');
+}
