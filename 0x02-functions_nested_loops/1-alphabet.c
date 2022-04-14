ee93a91c5f7249effa5b94d1b2f0b758f165ab41
Obianuju <vickielyon4@gmail.com>
2022-04-13 20:12:05 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lower case followed by new line 
+ * print_alphabet - prints alphabet in lower case followed by new line
  */
 void print_alphabet(void)
 {
